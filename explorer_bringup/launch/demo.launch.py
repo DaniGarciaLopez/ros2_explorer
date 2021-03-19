0de8391bb05fdaa8f75c7a49a864dde737440290
DaniGarciaLopez <d.garcialopez@hotmail.com>
2021-03-19 21:35:56 +0100

    CSV map update

--- explorer_bringup/launch/demo.launch.py
+++ explorer_bringup/launch/demo.launch.py
@@ -1,28 +1,61 @@
 #!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim
 
-"""Launch a Gazebo server with an empty world and initialize ROS with command line arguments."""
-import launch
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
-from launch_ros.actions import Node
-import os
-from pathlib import Path
-os.chdir(os.path.join(str(Path.home()), "turtlebot3_ws", "src", "ros2_explorer","explorer_gazebo"))
-def generate_launch_description():
-    # TODO(anyone): Forward command line arguments once that's supported, see
-    # https://github.com/ros2/launch/issues/107
-    gzserver = launch.actions.ExecuteProcess(
-        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',
-         'worlds/map1.world.xml'],
-        output='screen'
-    )
+from launch.substitutions import LaunchConfiguration
 
-    spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
-                arguments=['-entity', 'turtlebot3', '-file', 'models/turtlebot3_burger/model.sdf'],
-                output='screen')
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    world_file_name = 'map.world.xml'
+    world = os.path.join(get_package_share_directory('explorer_gazebo'),
+                         'worlds', world_file_name)
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     return LaunchDescription([
-        gzserver, spawn_entity
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            ),
+        ),
+
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo', 'use_sim_time', use_sim_time],
+            output='screen'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
     ])
--- explorer_gazebo/gazebo-map-from-csv.py
+++ explorer_gazebo/gazebo-map-from-csv.py
@@ -4,104 +4,144 @@
 # CopyPolicy: released under the terms of the LGPLv2.1
 # URL: https://github.com/roboticslab-uc3m/gazebo-tools
 
-#Modified from original to load all walls as a fixed model instead of loading every square as a model. Better performance.
+# Modified by: Daniel Garcia Lopez
+# URL: https://github.com/DaniGarciaLopez/ros2_explorer
+
+#Modified from original to load all walls as a fixed model instead of loading every square as a unique model. Better performance.
 
 from lxml import etree
+import os
 
-#-- User variables
-boxHeight = 1.0
-filename='map1'
-inFileStr = 'maps/'+filename+'.csv'
+#Get all names from csv files in 'maps' folder
+path_to_csv = 'maps/'
+filenames = [f for f in os.listdir(path_to_csv) if f.endswith('.csv')] #Get all filenames from folder ending in .csv
 
-resolution = 1.0  # Just to make similar to MATLAB [pixel/meter]
-meterPerPixel = 1 / resolution  # [meter/pixel]
+#Raise error if folder is empty
+if not filenames:
+    raise ValueError('No CSV files in maps folder')
 
-#-- Program
-from numpy import genfromtxt
-inFile = genfromtxt(inFileStr, delimiter=',')
-print(inFile)
+for filename in filenames: #Iterate through all csv filenames
+    filename=filename[:-4] #Delete .csv extension
+    #-- User variables
+    boxHeight = 1.0
+    inFileStr = 'maps/'+filename+'.csv'
 
-nX = inFile.shape[0]
-nY = inFile.shape[1]
-print("lines = X =",inFile.shape[0])
-print("columns = Y =",inFile.shape[1])
+    resolution = 1.0  # Just to make similar to MATLAB [pixel/meter]
+    meterPerPixel = 1 / resolution  # [meter/pixel]
 
-#-- Default to X=rows,Y=columns. Uncomment the next 3 lines to transpose.
-# print("transposing")
-# from numpy import transpose
-# inFile = transpose(inFile)
+    #-- Program
+    from numpy import genfromtxt
+    inFile = genfromtxt(inFileStr, delimiter=',')
+    print(inFile)
 
-Ez = boxHeight
+    nX = inFile.shape[0]
+    nY = inFile.shape[1]
+    print("lines = X =",inFile.shape[0])
+    print("columns = Y =",inFile.shape[1])
 
-Ex = meterPerPixel
-Ey = meterPerPixel
+    #-- Default to X=rows,Y=columns. Uncomment the next 3 lines to transpose.
+    # print("transposing")
+    # from numpy import transpose
+    # inFile = transpose(inFile)
 
-sdf = etree.Element("sdf", version="1.5")
-# world = etree.SubElement(sdf, "world", name="default")
-# light = etree.SubElement(world, "light", name="sun", type="directional")
-# cast_shadows = etree.SubElement(light, "cast_shadows").text="1"
-# diffuse = etree.SubElement(light, "diffuse").text="0.8 0.8 0.8 1"
-# specular = etree.SubElement(light, "specular").text="0.1 0.1 0.1 1"
-# attenuation = etree.SubElement(light, "attenuation")
-# _range = etree.SubElement(attenuation, "range").text="1000"
-# constant = etree.SubElement(attenuation, "constant").text="0.9"
-# linear = etree.SubElement(attenuation, "linear").text="0.01"
-# quadratic = etree.SubElement(attenuation, "quadratic").text="0.001"
-# direction = etree.SubElement(light, "direction").text="-0.5 0.5 -1"
+    Ez = boxHeight
 
-#-- Create Floor
-floorEx = Ex * nX
-floorEy = Ey * nY
-floorEz = boxHeight / 8.0  # arbitrary
+    Ex = meterPerPixel
+    Ey = meterPerPixel
 
-model = etree.SubElement(sdf, "model", name="map")
-static = etree.SubElement(model, "static").text="1"
-link = etree.SubElement(model, "link", name="map")
-collision = etree.SubElement(link, "collision", name="floor")
-pose = etree.SubElement(collision, "pose").text=str(floorEx/2.0)+" "+str(floorEy/2.0)+" "+str(-floorEz/2.0)+" 0 0 0"
-geometry = etree.SubElement(collision, "geometry")
-box = etree.SubElement(geometry, "box")
-size = etree.SubElement(box, "size").text=str(floorEx)+" "+ str(floorEy)+" "+str(floorEz)
-visual = etree.SubElement(link, "visual", name="floor")
-pose = etree.SubElement(visual, "pose").text=str(floorEx/2.0)+" "+str(floorEy/2.0)+" "+str(-floorEz/2.0)+" 0 0 0"
-geometry = etree.SubElement(visual, "geometry")
-box = etree.SubElement(geometry, "box")
-size = etree.SubElement(box, "size").text=str(floorEx)+" "+ str(floorEy)+" "+str(floorEz)
+    sdf = etree.Element("sdf", version="1.5")
+    # world = etree.SubElement(sdf, "world", name="default")
+    # light = etree.SubElement(world, "light", name="sun", type="directional")
+    # cast_shadows = etree.SubElement(light, "cast_shadows").text="1"
+    # diffuse = etree.SubElement(light, "diffuse").text="0.8 0.8 0.8 1"
+    # specular = etree.SubElement(light, "specular").text="0.1 0.1 0.1 1"
+    # attenuation = etree.SubElement(light, "attenuation")
+    # _range = etree.SubElement(attenuation, "range").text="1000"
+    # constant = etree.SubElement(attenuation, "constant").text="0.9"
+    # linear = etree.SubElement(attenuation, "linear").text="0.01"
+    # quadratic = etree.SubElement(attenuation, "quadratic").text="0.001"
+    # direction = etree.SubElement(light, "direction").text="-0.5 0.5 -1"
 
-#-- Create Walls
-for iX in range(nX):
-    #print("iX:",iX)
-    for iY in range(nY):
-        #print("* iY:",iY)
+    #-- Create Floor
+    floorEx = Ex * nX
+    floorEy = Ey * nY
+    floorEz = boxHeight / 8.0  # arbitrary
 
-        #-- Skip box if map indicates a 0
-        if inFile[iX][iY] == 0:
-            continue
+    model = etree.SubElement(sdf, "model", name="map")
+    static = etree.SubElement(model, "static").text="1"
+    link = etree.SubElement(model, "link", name="map")
+    collision = etree.SubElement(link, "collision", name="floor")
+    pose = etree.SubElement(collision, "pose").text=str(floorEx/2.0)+" "+str(floorEy/2.0)+" "+str(-floorEz/2.0)+" 0 0 0"
+    geometry = etree.SubElement(collision, "geometry")
+    box = etree.SubElement(geometry, "box")
+    size = etree.SubElement(box, "size").text=str(floorEx)+" "+ str(floorEy)+" "+str(floorEz)
+    visual = etree.SubElement(link, "visual", name="floor")
+    pose = etree.SubElement(visual, "pose").text=str(floorEx/2.0)+" "+str(floorEy/2.0)+" "+str(-floorEz/2.0)+" 0 0 0"
+    geometry = etree.SubElement(visual, "geometry")
+    box = etree.SubElement(geometry, "box")
+    size = etree.SubElement(box, "size").text=str(floorEx)+" "+ str(floorEy)+" "+str(floorEz)
 
-        #-- Add E___/2.0 to each to force begin at 0,0,0 (centered by default)
-        x = Ex/2.0 + iX*meterPerPixel
-        y = Ey/2.0 + iY*meterPerPixel
-        z = Ez/2.0  # Add this to raise to floor level (centered by default)
+    #-- Create Walls
+    for iX in range(nX):
+        #print("iX:",iX)
+        for iY in range(nY):
+            #print("* iY:",iY)
 
-        #-- Create box
-        name="box_"+str(iX)+"_"+str(iY)
-        #model = etree.SubElement(world, "model", name="box_"+str(iX)+"_"+str(iY))
-        #static = etree.SubElement(model, "static").text="true"
-        #link = etree.SubElement(model, "link", name="box_"+str(iX)+"_"+str(iY))
-        collision = etree.SubElement(link, "collision", name=name)
-        pose = etree.SubElement(collision, "pose").text=str(x)+" "+str(y)+" "+str(z)+" 0 0 0"
-        geometry = etree.SubElement(collision, "geometry")
-        box = etree.SubElement(geometry, "box")
-        size = etree.SubElement(box, "size").text=str(Ex)+" "+ str(Ey)+" "+str(Ez)
-        
-        visual = etree.SubElement(link, "visual", name=name)
-        pose = etree.SubElement(visual, "pose").text=str(x)+" "+str(y)+" "+str(z)+" 0 0 0"
-        geometry = etree.SubElement(visual, "geometry")
-        box = etree.SubElement(geometry, "box")
-        size = etree.SubElement(box, "size").text=str(Ex)+" "+ str(Ey)+" "+str(Ez)
+            #-- Skip box if map indicates a 0
+            if inFile[iX][iY] == 0:
+                continue
 
-myStr = etree.tostring(sdf, pretty_print=True, encoding="unicode")
+            #-- Add E___/2.0 to each to force begin at 0,0,0 (centered by default)
+            x = Ex/2.0 + iX*meterPerPixel
+            y = Ey/2.0 + iY*meterPerPixel
+            z = Ez/2.0  # Add this to raise to floor level (centered by default)
 
-outFile = open(filename+'.sdf', 'w')
-outFile.write(myStr)
-outFile.close()
+            #-- Create box
+            name="box_"+str(iX)+"_"+str(iY)
+            #model = etree.SubElement(world, "model", name="box_"+str(iX)+"_"+str(iY))
+            #static = etree.SubElement(model, "static").text="true"
+            #link = etree.SubElement(model, "link", name="box_"+str(iX)+"_"+str(iY))
+            collision = etree.SubElement(link, "collision", name=name)
+            pose = etree.SubElement(collision, "pose").text=str(x)+" "+str(y)+" "+str(z)+" 0 0 0"
+            geometry = etree.SubElement(collision, "geometry")
+            box = etree.SubElement(geometry, "box")
+            size = etree.SubElement(box, "size").text=str(Ex)+" "+ str(Ey)+" "+str(Ez)
+            
+            visual = etree.SubElement(link, "visual", name=name)
+            pose = etree.SubElement(visual, "pose").text=str(x)+" "+str(y)+" "+str(z)+" 0 0 0"
+            geometry = etree.SubElement(visual, "geometry")
+            box = etree.SubElement(geometry, "box")
+            size = etree.SubElement(box, "size").text=str(Ex)+" "+ str(Ey)+" "+str(Ez)
+
+    myStr = etree.tostring(sdf, pretty_print=True, encoding="unicode")
+
+    #Create map folder
+    path=os.path.join('models', filename)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    path=os.path.join('models', filename)
+
+    #Write .sdf file
+    outFile = open(os.path.join(path, filename+'.sdf'), 'w')
+    outFile.write(myStr)
+    outFile.close()
+
+    #Create model.config file
+    model = etree.Element("model")
+    name = etree.SubElement(model, "name").text='CSV Map'
+    version = etree.SubElement(model, "version").text='1.0'
+    sdf = etree.SubElement(model, "sdf", version='1.5').text=filename+'.sdf'
+
+    # author = etree.SubElement(model, "author")
+    # name = etree.SubElement(author, "name").text=''
+    # name = etree.SubElement(author, "email").text=''
+
+    description = etree.SubElement(model, "description").text='Map generated automatically from a csv file. More info: https://github.com/DaniGarciaLopez/ros2_explorer'
+
+    #Write model.config file
+    myStr = etree.tostring(model, pretty_print=True, encoding="unicode")
+    outFile = open(os.path.join(path, 'model.config'), 'w')
+    outFile.write(myStr)
+    outFile.close()
+
+
