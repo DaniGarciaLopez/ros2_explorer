1fbc7ef83272c1cb460dc1d6779c7cdbdad38cec
DaniGarciaLopez <d.garcialopez@hotmail.com>
2021-05-14 21:45:58 +0200

    Add navigation package

---
+++ explorer_navigation2/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'explorer_navigation2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dani',
+    maintainer_email='d.garcialopez@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
